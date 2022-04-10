8c0c2c027954db4c8f02466e11b89ff96951062a
cleavecamael <phoenixlee125@gmail.com>
2022-04-11 00:17:29 +0800

    Added add Task popup

--- app/src/main/java/com/example/studify/views/TaskListFragment.java
+++ app/src/main/java/com/example/studify/views/TaskListFragment.java
@@ -37,20 +37,21 @@ public class TaskListFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentTaskListBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-        String[] s = {"Hello", "World","yes"};
-        String[] t = {"2", "3","4"};
+        String[] s = {"Hello", "World", "yes"};
+        String[] t = {"2", "3", "4"};
         recycleView = binding.tasksRecyclerView;
         TaskAdapter adapter = new TaskAdapter(getActivity(), s, t);
         recycleView.setAdapter(adapter);
         recycleView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        int id = view.getId();
 
         return view;
-
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        binding.addButton.setOnClickListener(this);
 
     }
 
@@ -58,7 +59,22 @@ public class TaskListFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         int id = view.getId();
+        if (id == binding.addButton.getId()) {
+            System.out.println("yes");
+            int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+            int height = Resources.getSystem().getDisplayMetrics().heightPixels;
 
+            View popupView = getLayoutInflater().inflate(R.layout.add_task_popup, null);
+            PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
+            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+            //close the popup window when cross is clicked
+            ImageButton close = popupView.findViewById(R.id.closeButton);
+            close.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View popupView) {
+                    popupWindow.dismiss();
+                }
+            });
+        }
 
         }
 
